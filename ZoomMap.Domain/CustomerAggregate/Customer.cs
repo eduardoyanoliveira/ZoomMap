4b91232345c135aa23ccb59b9d27fe9f98a025c9
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2023-02-18 23:08:17 -0300

    Add CPF and Address ValueObjects to Customer

--- ZoomMap.Domain/CustomerAggregate/Customer.cs
+++ ZoomMap.Domain/CustomerAggregate/Customer.cs
@@ -1,4 +1,6 @@
 ﻿using ZoomMap.Domain.Common.Models;
+using ZoomMap.Domain.Common.ValueObjects.AddressValueObject;
+using ZoomMap.Domain.Common.ValueObjects.CPFValueObject;
 using ZoomMap.Domain.ContractAggregate.ValueObjects;
 using ZoomMap.Domain.CustomerAggregate.ValueObjects;
 using ZoomMap.Domain.ServiceAppointmentAggregate.ValueObjects;
@@ -13,41 +15,49 @@ namespace ZoomMap.Domain.CustomerAggregate
         private List<ServiceAppointmentId> _serviceAppointments = new();
         public IReadOnlyList<ServiceAppointmentId> ServiceAppointments => _serviceAppointments.AsReadOnly();
 
-        // Todo: CPF
+        public  CPF CPF { get; }
         public string Name { get; }
         public string Surname { get; }
         public DateTime BirthDate { get; }
         public string Email { get; }
-        // Todo: Address
+        public Address Address { get; }
 
         private Customer(
             CustomerId customerId,
+            CPF cpf,
             string name,
             string surname,
             DateTime birthDate,
-            string email
+            string email,
+            Address address
         )
             :base(customerId)
         {
+            CPF= cpf;
             Name= name;
             Surname= surname;
             BirthDate = birthDate;
             Email = email;
+            Address= address;
         }
 
         public static Customer Create(
+            CPF cpf,
             string name,
             string surname,
             DateTime birthDate,
-            string email
+            string email,
+            Address address
         )
         {
             return new Customer(
                CustomerId.CreateUnique(),
+               cpf,
                name,
                surname,
                birthDate,
-               email
+               email,
+               address
             );
         }
     }
